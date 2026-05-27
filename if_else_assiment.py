3526834787284bfb720b05ef818482b23fa39ac3
Deepak Aswani <134124125+Deepak-18449@users.noreply.github.com>
2026-05-27 11:45:39 +0530

    if_else_ass

---
+++ if_else_assiment.py
@@ -0,0 +1,36 @@
+a=int(input("enter a number"))
+if a%2==0:
+  print("even number")
+else:
+  print("odd number")
+
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+if a>b and a>c:
+  print("a is greater")
+elif b>a and b>c:
+  print("b is greater")
+else:
+  print("c is greater")
+
+a=2004
+if a==100:
+  print("not leap year")
+elif a%4==0:
+  print("leap year")
+else:
+  print("not a leap year")
+
+a=int(input("enter a number"))
+if a>=90:
+  print("A")
+elif a>=80:
+  print("B")
+elif a>=70:
+  print("C")
+elif a>=60:
+  print("D")
+else:
+  print("E")
+
